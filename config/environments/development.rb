46e9b98ab0dc12f71efecc31080718933d3fdbd4
Naren Kukreja <narenkukreja@gmail.com>
2016-02-15 15:47:45 -0500

    Assets recompile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,9 +9,6 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  config.assets.raise_asset_errors = true
-
-
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
@@ -30,10 +27,6 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
-  # yet still be able to expire them through the digest params.
-  config.assets.digest = true
-
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
@@ -41,6 +34,4 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.assets.compile = true
-
 end
